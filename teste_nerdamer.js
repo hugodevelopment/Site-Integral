f51c2f57be706060d4bf4a1705e51f2c9099ad91
hugodevelopment <alveshugo2906@gmail.com>
2023-11-17 15:13:58 -0300

    ok

--- teste_nerdamer.js
+++ teste_nerdamer.js
@@ -1,12 +1,12 @@
 
 // Defina a expressão que você deseja integrar
 const expressao = '1/x';
-const exp = "3x"
+const exp = "3x + 1"
 
 // Realize a integração
 const nerdamer = require('./nerdamer/all');
 const resultado = nerdamer.integrate(expressao, 'x');
-const resultado1 = nerdamer.integrate(exp, 'x');
+const resultado1 = nerdamer.diff(exp, 'x');
 // Exiba o resultado
 console.log("resposta é: ", resultado.toString());
 console.log("resposta é: ", resultado1.toString());
